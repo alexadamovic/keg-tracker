e636f1528171a11cef163506a4538cfa0f5d3b6e
Alex Adamovic <alexanderadamovic@gmail.com>
2022-04-22 09:14:19 -0700

    Add components folder and edit code in App.js for current project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import KegControl from "./KegControl";
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Container>
+        <Header />
+        <KegControl />
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
