b5ce834e9adc5daef84a18068907df5c9602fec0
Alex Adamovic <alexanderadamovic@gmail.com>
2022-04-22 09:42:27 -0700

    Add Keg component with basic bootstrap card layout

---
+++ src/components/Keg.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+
+function Keg(props){
+
+  const kegCardStyles = {
+    backgroundColor: '#dfebf5',
+    width: '18rem',
+    border: 'solid 1px'
+  }
+
+  return (
+    <React.Fragment>
+      <Col>
+        <div onClick = {() => props.whenKegClicked(props.id)}>
+          <Card style={kegCardStyles}>
+            <Card.Img variant="top" src="../img/keg.jpg" />
+            <Card.Body>
+              <Card.Title>{props.name} - {props.brand}</Card.Title>
+              <Card.Text>
+                <p>{props.style} - <em>{props.abv}% abv</em></p>
+                <p><em>${props.price}</em></p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>  
+      </Col>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  style: PropTypes.string,isRequired,
+  price: PropTypes.number.isRequired,
+  abv: PropTypes.number.isRequired,
+  whenKegClicked: PropTypes.func
+};
+
+export default Keg;
